e0cad214e2415a8054a76df84501217db0012132
oscar welton <oscarwelton@gmail.com>
2023-08-15 21:52:42 +0100

    scroll effect completed. Need to add more img paths

--- index.js
+++ index.js
@@ -6,20 +6,27 @@ document.addEventListener("DOMContentLoaded", function () {
   const layerFive = document.getElementById("layer-5");
   const layerSix = document.getElementById("layer-6");
 
+  const banner = document.querySelector(".banner");
+
+  const updateBanner = () => {
+    const scroll = window.pageYOffset;
+    banner.style.transform = `rotate3d(2, 3, 1, -${scroll * 0.05}deg) translate(-${scroll * 0.05}px, -${scroll * 0.05}px)`;
+  };
 
   const updateParallax = () => {
     const scroll = window.pageYOffset;
     const scroll2 = window.pageYOffset;
     const scroll3 = window.pageYOffset;
 
-    layerOne.setAttribute("transform", `translate(${0}, ${-(scroll * 0.1).toFixed(0)})`);
-    layerTwo.setAttribute("transform", `translate(${0}, ${(scroll2 * 0.2).toFixed(0)})`);
-    layerThree.setAttribute("transform", `translate(${0}, ${(scroll3 * 0.3).toFixed(0)})`);
-    layerFour.setAttribute("transform", `translate(${0}, ${-(scroll * 0.4).toFixed(0)})`);
-    layerFive.setAttribute("transform", `translate(${0}, ${-(scroll2 * 0.5).toFixed(0)})`);
-    layerSix.setAttribute("transform", `translate(${0}, ${-(scroll3 * 0.6).toFixed(0)})`);
+    layerOne.setAttribute("transform", `translate(${-(scroll * 0.1).toFixed(0)}, 0)`);
+    layerTwo.setAttribute("transform", `translate(${(scroll2 * 0.2).toFixed(0)}, 0)`);
+    layerThree.setAttribute("transform", `translate(-${(scroll3 * 0.3).toFixed(0)} ,0)`);
+    layerFour.setAttribute("transform", `translate(${-(scroll * 0.4).toFixed(0)}, 0)`);
+    layerFive.setAttribute("transform", `translate(${-(scroll2 * 0.5).toFixed(0)}, 0)`);
+    layerSix.setAttribute("transform", `translate(${-(scroll3 * 0.6).toFixed(0)}, 0)`);
   };
 
+  // window.addEventListener("scroll", updateBanner);
   window.addEventListener("scroll", updateParallax);
   window.addEventListener("resize", updateParallax);
   updateParallax();

afe4a1de9e098579abcaeb484d14f563e321af07
oscar welton <oscarwelton@gmail.com>
2023-08-22 13:42:34 +0100

    fix: banner hidden / visible toggle on scroll

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ document.addEventListener("DOMContentLoaded", function () {
   const layerSeven = document.getElementById("layer-7");
 
   const header = document.querySelector(".header");
+  const headerDiv = document.querySelector(".header-div")
   const banner = document.querySelector(".banner");
   const initialFontSize = parseFloat(getComputedStyle(header).fontSize);
   const initialHeight = banner.offsetHeight;
@@ -28,19 +29,39 @@ document.addEventListener("DOMContentLoaded", function () {
     const newSize = Math.max(initialFontSize, initialFontSize + scroll);
 
     if (scroll < 500) {
-      banner.style.height = initialHeight - scroll + "px";
-      header.style.transform = `translateY(-${scroll / 3}px)`;
+      // banner.style.transform = `translateY(-${scroll / 3}px)`
+      // headerDiv.style.transform = `translateY(-${scroll / 3}px)`;
       header.style.fontSize = `${newSize}px`;
       header.style.opacity = 1 - scroll / 300;
+      banner.style.overflow = 'hidden'
 
       if (header.style.opacity < 0) {
         header.style.opacity = 0;
+      }
+
+      if (header.style.opacity > 0) {
+        banner.style.overflow = 'visible'
       }
     }
   };
 
 
+  const services = document.querySelectorAll('.service');
+  function fadeInOnScroll() {
+    services.forEach(service => {
+      const serviceTop = service.getBoundingClientRect().top;
+      const windowHeight = window.innerHeight;
 
+      if (serviceTop + 200 < windowHeight) {
+        service.style.opacity = 1;
+        service.style.transform = 'translateX(0)';
+      }
+    });
+  }
+
+  window.addEventListener('scroll', fadeInOnScroll);
+  window.addEventListener('resize', fadeInOnScroll);
+  fadeInOnScroll();
 
   const updateParallax = () => {
     const scroll = window.pageYOffset;

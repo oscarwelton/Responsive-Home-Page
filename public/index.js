7873e4fea2f908e036bf92c244490fbfff9b0305
oscar welton <oscarwelton@gmail.com>
2023-09-05 11:45:55 +0100

    contact animations working

--- public/index.js
+++ public/index.js
@@ -2,21 +2,27 @@ window.addEventListener("load", () => {
   let contactOpen = false;
   const contact = document.querySelector(".contact");
   const form = document.querySelector(".form");
-  const contactForm = document.querySelector(".form-container");
+  const formContainer = document.querySelector(".form-container");
+  const path = document.getElementById("svg-path")
 
   contact.addEventListener("click", () => {
     if (!contactOpen) {
-      contactForm.style.display = "block";
+      formContainer.style.display = "flex";
       form.classList.add("in");
       form.classList.remove("out");
       contact.innerHTML = "CLOSE";
       contactOpen = true;
+      formContainer.style.backgroundColor = "rgba(255, 255, 255, 0.75)";
+      path.classList.remove("undraw");
+      path.classList.add("draw");
     } else {
       form.classList.remove("in");
+      path.classList.remove("draw");
+      path.classList.add("undraw");
       form.classList.add("out");
+      formContainer.style.backgroundColor = "rgba(255, 255, 255, 0)";
       contact.innerHTML = "LET'S <br> TALK";
       contactOpen = false;
-      // contactForm.style.display = "none";
     }
   });
 

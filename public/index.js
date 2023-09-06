aabb095a9ebe69b8a69f9ab57dbabd58567ba58d
oscar welton <oscarwelton@gmail.com>
2023-09-06 13:22:06 +0100

    refactored js

--- public/index.js
+++ public/index.js
@@ -4,52 +4,55 @@ window.addEventListener("load", () => {
   const formContainer = document.querySelector(".form-container");
   const path = document.getElementById("path");
   const closeButton = document.getElementById("close");
-
+  const navContact = document.getElementById("nav-contact");
   const contactButton = document.querySelector(".contact-button");
+
+  function openForm() {
+    if (window.innerWidth <= 650) {
+      contactButton.classList.add("fade-out");
+    }
+    formContainer.style.display = "flex";
+    formContainer.style.backgroundColor = "rgba(255, 255, 255, 0.5)";
+    form.classList.add("in");
+    form.classList.remove("out");
+    contactOpen = true;
+    path.classList.remove("undraw");
+    path.classList.add("draw");
+  }
+
+  function closeForm() {
+    form.classList.remove("in");
+    formContainer.style.backgroundColor = "rgba(0, 0, 0, 0)";
+    path.classList.remove("draw");
+    path.classList.add("undraw");
+    form.classList.add("out");
+    contactOpen = false;
+    setTimeout(() => {
+      formContainer.style.display = "none";
+    }, 1500);
+  }
 
   contactButton.addEventListener("click", () => {
     if (!contactOpen) {
-      if (window.innerWidth <= 650) {
-        contactButton.classList.add("fade-out");
-      }
-      formContainer.style.display = "flex";
-      form.classList.add("in");
-      form.classList.remove("out");
-      contactOpen = true;
-      path.classList.remove("undraw");
-      path.classList.add("draw");
+      openForm();
     } else {
-      form.classList.remove("in");
-      path.classList.remove("draw");
-      path.classList.add("undraw");
-      form.classList.add("out");
-      contactOpen = false;
-      setTimeout(() => {
-        formContainer.style.display = "none";
-      }, 1500);
+      closeForm();
     }
   });
 
   closeButton.addEventListener("click", () => {
     if (!contactOpen) {
-      if (window.innerWidth <= 650) {
-        contactButton.classList.add("fade-out");
-      }
-      formContainer.style.display = "flex";
-      form.classList.add("in");
-      form.classList.remove("out");
-      contactOpen = true;
-      path.classList.remove("undraw");
-      path.classList.add("draw");
+      openForm();
     } else {
-      form.classList.remove("in");
-      path.classList.remove("draw");
-      path.classList.add("undraw");
-      form.classList.add("out");
-      contactOpen = false;
-      setTimeout(() => {
-        formContainer.style.display = "none";
-      }, 1500);
+      closeForm();
+    }
+  });
+
+  navContact.addEventListener("click", () => {
+    if (!contactOpen) {
+      openForm();
+    } else {
+      closeForm();
     }
   });
 });
